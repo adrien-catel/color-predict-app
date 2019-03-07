9e946973bac956e243588e72b24ba6defafff13e
Adrien Catel <adrien.catel@nist.gov>
2019-03-07 14:02:59 -0500

    feat: redux-thunk implementation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 import { 
+  USER_POSITION_LOADED,
   SET_FIRST_PREDICTION,
-  SET_SECOND_PREDICTION 
+  SET_SECOND_PREDICTION,
 } from "../constants/action-types";
+
+import DarkSkyApi from 'dark-sky-api';
 
 export function setFirstPrediction(payload) {
   return { type: SET_FIRST_PREDICTION, payload }
@@ -10,3 +13,12 @@ export function setFirstPrediction(payload) {
 export function setSecondPrediction(payload) {
   return { type: SET_SECOND_PREDICTION, payload }
 };
+
+export function loadUserPosition() {
+  return function(dispatch) {
+    return DarkSkyApi.loadPosition()
+      .then(pos => {
+          dispatch({ type: USER_POSITION_LOADED, payload: pos });
+      });
+  }
+}
