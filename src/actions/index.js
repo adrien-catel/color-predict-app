ff569e7ccbaac9fc4d1d29fcfe2bab9b19b10e6b
Adrien Catel <adrien.catel@nist.gov>
2019-03-20 11:53:42 -0400

    refactor: Axios called in Actions + Prediction object modification

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,65 @@
+import DarkSkyApi from 'dark-sky-api';
+import axios from "axios";
+
 import { 
   USER_POSITION_LOADED,
   SET_FIRST_PREDICTION,
   SET_SECOND_PREDICTION,
 } from "../constants/action-types";
 
-import DarkSkyApi from 'dark-sky-api';
-
-export function setFirstPrediction(payload) {
-  return { type: SET_FIRST_PREDICTION, payload }
-};
-
-export function setSecondPrediction(payload) {
-  return { type: SET_SECOND_PREDICTION, payload }
-};
+import { Prediction } from "../utils/Prediction/Prediction";
 
 export function loadUserPosition() {
   return function(dispatch) {
     return DarkSkyApi.loadPosition()
       .then(pos => {
         dispatch({ type: USER_POSITION_LOADED, payload: pos });
+      });
+  }
+}
+
+export function loadFirstPrediction(date, position) {
+  return loadPrediction(date, position, SET_FIRST_PREDICTION);
+}
+
+export function loadSecondPrediction(date, position) {
+  return loadPrediction(date, position, SET_SECOND_PREDICTION);
+}
+
+export function loadPrediction(date, position, type) {
+  return function(dispatch) {
+    axios.get("/api/darksky/loadtime/", {
+        params: { 
+          latitude: position.latitude, 
+          longitude: position.longitude, 
+          moment: date
+        }
+      })
+      .then(response => {
+        const condition = response.data.currently;
+        let prediction = new Prediction(date, 
+                                        condition.cloudCover,
+                                        condition.windSpeed,
+                                        condition.humidity,
+                                        condition.visibility,
+                                        condition.summary,
+                                        condition.icon);
+        
+        const content = {
+          title: prediction.getTitle(),
+          windSpeed: prediction.criteriaWind.value,
+          visibility: prediction.criteriaVisibility.value,
+          humidity: prediction.criteriaHumidity.value,
+          cloudCover: prediction.criteriaCloud.value,
+          prediction: prediction.getResult(),
+          summary: prediction.summary,
+          icon: prediction.icon,
+          isReady: true,
+        }
+        dispatch({ type: type, payload: content });
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 }
--- src/utils/Prediction/Prediction.js
+++ src/utils/Prediction/Prediction.js
@@ -1,10 +1,35 @@
+import { CriteriaCloud } from "../Criteria/CriteriaCloud";
+import { CriteriaHumidity } from "../Criteria/CriteriaHumidity";
+import { CriteriaVisibility } from "../Criteria/CriteriaVisibility";
+import { CriteriaWind } from "../Criteria/CriteriaWind";
+import { IsMorning } from "../../utils/Date";
+
 export class Prediction {
-  /** Represent a Prediction
-   * 
-   * @param {*} criteriaList 
+  /**
+   *  Represent a Prediction 
    */
-  constructor(criteriaList) {
-    this.criteriaList = criteriaList;
+  constructor(date, 
+              cloudCoverValue, 
+              windSpeedValue, 
+              humidityValue, 
+              visibilityValue, 
+              summary, 
+              icon) {
+    this.date = date;
+    this.criteriaCloud = new CriteriaCloud(Math.floor(cloudCoverValue*100));
+    this.criteriaWind = new CriteriaWind(Math.floor(windSpeedValue));
+    this.criteriaHumidity = new CriteriaHumidity(Math.floor(humidityValue*100));
+    this.criteriaVisibility = new CriteriaVisibility(Math.floor(visibilityValue));
+    this.summary = summary;
+    this.icon = icon;
+  }
+
+  /**
+   * Return the prediction title
+   * from the date
+   */
+  getTitle() {
+    return IsMorning(this.date) ? "Sunrise - " + this.date.toLocaleString() : "Sunset - " + this.date.toLocaleString();
   }
 
   /**
@@ -14,7 +39,15 @@ export class Prediction {
   getResult() {
     var nominator = 0;
     var denominator = 0;
-    this.criteriaList.forEach((criteria) => {
+
+    const criteriaList = [
+      this.criteriaCloud,
+      this.criteriaWind,
+      this.criteriaHumidity,
+      this.criteriaVisibility
+    ]
+
+    criteriaList.forEach((criteria) => {
       nominator += criteria.getScore() * criteria.weight;
       denominator += criteria.weight;
     });
